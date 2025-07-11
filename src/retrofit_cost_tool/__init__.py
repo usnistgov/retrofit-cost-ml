f2d2936209e12518a202e0c69900104b42b9234f
Juan F. Fung <juan.fung@nist.gov>
2025-07-11 10:43:55 -0400

    Update __init__.py to include OLS and Gamma GLM training functions

--- src/retrofit_cost_tool/__init__.py
+++ src/retrofit_cost_tool/__init__.py
@@ -1,6 +1,6 @@
 # retrofit_cost_tool/__init__.py
 from .data_utils import load_data, preprocess_data, split_data
-from .model_utils import train_ridge_model, train_elastic_net_model, train_random_forest_model, train_gradient_boosting_model, evaluate_model
+from .model_utils import train_ridge_model, train_elastic_net_model, train_random_forest_model, train_gradient_boosting_model, train_ols_model, train_glm_gamma_model, evaluate_model
 from .model_io import save_model, load_model
 from .model_selection import model_selection
 from .main import main
